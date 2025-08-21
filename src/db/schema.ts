4fc15d4c47cdcf6fb39e4c52fd23d80f3d4d156a
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-08-21 16:59:15 +0530

    type fix

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,28 +9,14 @@ import {
   jsonb
 } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
+import type { GestureData } from '~/tools/stroke_data/types';
 
 export const text_data = pgTable('text_data', {
   id: serial('id').primaryKey(),
   uuid: uuid('uuid').notNull().defaultRandom(),
   text: text('text').notNull(),
   svg_json: jsonb('svg_json').notNull().$type<any>(),
-  strokes_json: jsonb('strokes_json').$type<{
-    gestures: {
-      order: number;
-      strokes: {
-        order: number;
-        points: {
-          x: number;
-          y: number;
-          timestamp: number;
-        }[];
-      }[];
-      brush_width: number;
-      brush_color: string;
-      animation_duration: number;
-    }[];
-  }>(),
+  strokes_json: jsonb('strokes_json').$type<GestureData>(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at')
     .notNull()

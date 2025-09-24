8740bab6c0eeb95f95d93e2f5948b1a34a7c221e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-09-24 22:35:16 +0530

    font added

--- src/state/font_list.ts
+++ src/state/font_list.ts
@@ -1,18 +1,32 @@
 import { type script_list_type } from './lang_list';
 
 export const FONT_FAMILIES = [
+  // All
   'Nirmala_UI',
+  // Devanagari
   'Adobe_Devanagari',
+  'Noto_Serif_Devanagari',
+  'Noto_Sans_Devanagari',
+  // Telugu
   'Adobe_Telugu',
   'Noto_Serif_Telugu',
-  'Noto_Serif_Kannada'
+  'Noto_Sans_Telugu',
+  // Kannada
+  'Noto_Serif_Kannada',
+  'Noto_Sans_Kannada',
+  // Odia
+  'Noto_Serif_Odia',
+  'Noto_Sans_Odia',
+  // Malayalam
+  'Noto_Serif_Malayalam',
+  'Noto_Sans_Malayalam'
 ] as const;
 export type FontFamily = (typeof FONT_FAMILIES)[number];
 
 /**
  * These are the scripts for which the fonts have been specified and setup properly
  */
-export const FONT_SCRIPTS = ['Devanagari', 'Telugu', 'Kannada'] as const;
+export const FONT_SCRIPTS = ['Devanagari', 'Telugu', 'Kannada', 'Odia', 'Malayalam'] as const;
 type FontEntry<F extends FontFamily> = {
   font_family: F;
   url: `/fonts/regular/woff2/${F}.woff2` | `/fonts/variable/woff2/${F}.woff2`;
@@ -29,16 +43,30 @@ const get_font_entry = (font_family: FontFamily, loc: 'regular' | 'variable') =>
 export const FONT_LIST: FontList = {
   Devanagari: [
     get_font_entry('Nirmala_UI', 'regular'),
-    get_font_entry('Adobe_Devanagari', 'regular')
+    get_font_entry('Adobe_Devanagari', 'regular'),
+    get_font_entry('Noto_Serif_Devanagari', 'variable'),
+    get_font_entry('Noto_Sans_Devanagari', 'variable')
   ],
   Telugu: [
     get_font_entry('Nirmala_UI', 'regular'),
     get_font_entry('Adobe_Telugu', 'regular'),
-    get_font_entry('Noto_Serif_Telugu', 'variable')
+    get_font_entry('Noto_Serif_Telugu', 'variable'),
+    get_font_entry('Noto_Sans_Telugu', 'variable')
   ],
   Kannada: [
     get_font_entry('Nirmala_UI', 'regular'),
-    get_font_entry('Noto_Serif_Kannada', 'variable')
+    get_font_entry('Noto_Serif_Kannada', 'variable'),
+    get_font_entry('Noto_Sans_Kannada', 'variable')
+  ],
+  Odia: [
+    get_font_entry('Nirmala_UI', 'regular'),
+    get_font_entry('Noto_Serif_Odia', 'variable'),
+    get_font_entry('Noto_Sans_Odia', 'variable')
+  ],
+  Malayalam: [
+    get_font_entry('Nirmala_UI', 'regular'),
+    get_font_entry('Noto_Serif_Malayalam', 'variable'),
+    get_font_entry('Noto_Sans_Malayalam', 'variable')
   ]
 };
 

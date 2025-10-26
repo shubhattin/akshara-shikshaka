d20e159a8459db1b9c05f14d07205a8e9c0dee2e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-10-26 17:56:23 +0530

    init

---
+++ src/db/redis.ts
@@ -0,0 +1,10 @@
+import { Redis } from '@upstash/redis';
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN
+});
+
+export const REDIS_CACHE_KEYS = {
+  //
+};

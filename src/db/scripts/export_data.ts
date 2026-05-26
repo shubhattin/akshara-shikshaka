2cab0fe4cb6ba75bd4c191dd54549fdbec65f563
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-05-26 21:01:40 +0530

    transaction in data push

--- src/db/scripts/export_data.ts
+++ src/db/scripts/export_data.ts
@@ -1,4 +1,4 @@
-import { dbClient_ext as db, queryClient } from './client';
+import { dbClient_ext, queryClient } from './client';
 import { readFile } from 'fs/promises';
 import { dbMode, take_input } from '~/tools/kry.server';
 import {
@@ -63,189 +63,191 @@ const main = async () => {
     })
     .parse(JSON.parse((await readFile(`./out/${in_file_name}`)).toString()));
 
-  // deleting all the tables initially
-  try {
-    await db.delete(text_gestures);
-    await db.delete(text_lessons);
-    await db.delete(gesture_text_key_category_join);
-    await db.delete(gesture_categories);
-    await db.delete(lesson_categories);
-    await db.delete(lesson_gestures);
-    await db.delete(text_lesson_words);
-    await db.delete(audio_assets);
-    await db.delete(image_assets);
-    await db.delete(user_gesture_recordings);
-    await db.delete(user_gesture_recording_vectors);
-    console.log(chalk.green('✓ Deleted All Tables Successfully'));
-  } catch (e) {
-    console.log(chalk.red('✗ Error while deleting tables:'), chalk.yellow(e));
-  }
+  dbClient_ext.transaction(async (tx) => {
+    // deleting all the tables initially
+    try {
+      await tx.delete(text_gestures);
+      await tx.delete(text_lessons);
+      await tx.delete(gesture_text_key_category_join);
+      await tx.delete(gesture_categories);
+      await tx.delete(lesson_categories);
+      await tx.delete(lesson_gestures);
+      await tx.delete(text_lesson_words);
+      await tx.delete(audio_assets);
+      await tx.delete(image_assets);
+      await tx.delete(user_gesture_recordings);
+      await tx.delete(user_gesture_recording_vectors);
+      console.log(chalk.green('✓ Deleted All Tables Successfully'));
+    } catch (e) {
+      console.log(chalk.red('✗ Error while deleting tables:'), chalk.yellow(e));
+    }
 
-  // inserting audio assets
-  try {
-    await db.insert(audio_assets).values(data.audio_assets);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`audio_assets`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting audio assets:'), chalk.yellow(e));
-  }
+    // inserting audio assets
+    try {
+      await tx.insert(audio_assets).values(data.audio_assets);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`audio_assets`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting audio assets:'), chalk.yellow(e));
+    }
 
-  // inserting image assets
-  try {
-    await db.insert(image_assets).values(data.image_assets);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`image_assets`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting image assets:'), chalk.yellow(e));
-  }
+    // inserting image assets
+    try {
+      await tx.insert(image_assets).values(data.image_assets);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`image_assets`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting image assets:'), chalk.yellow(e));
+    }
 
-  // inserting gesture categories
-  try {
-    await db.insert(gesture_categories).values(data.gesture_categories);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`gesture_categories`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting gesture categories:'), chalk.yellow(e));
-  }
+    // inserting gesture categories
+    try {
+      await tx.insert(gesture_categories).values(data.gesture_categories);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`gesture_categories`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting gesture categories:'), chalk.yellow(e));
+    }
 
-  // inserting text gestures
-  try {
-    await db
-      .insert(text_gestures)
-      .values(data.text_gestures as InferInsertModel<typeof text_gestures>[]);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`text_gestures`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting word_puzzles:'), chalk.yellow(e));
-  }
+    // inserting text gestures
+    try {
+      await tx
+        .insert(text_gestures)
+        .values(data.text_gestures as InferInsertModel<typeof text_gestures>[]);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`text_gestures`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting word_puzzles:'), chalk.yellow(e));
+    }
 
-  // inserting gesture text key category join
-  try {
-    await db.insert(gesture_text_key_category_join).values(data.gesture_text_key_category_join);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`gesture_text_key_category_join`')
-    );
-  } catch (e) {
-    console.log(
-      chalk.red('✗ Error while inserting gesture text key category join:'),
-      chalk.yellow(e)
-    );
-  }
+    // inserting gesture text key category join
+    try {
+      await tx.insert(gesture_text_key_category_join).values(data.gesture_text_key_category_join);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`gesture_text_key_category_join`')
+      );
+    } catch (e) {
+      console.log(
+        chalk.red('✗ Error while inserting gesture text key category join:'),
+        chalk.yellow(e)
+      );
+    }
 
-  // inserting lesson categories
-  try {
-    await db.insert(lesson_categories).values(data.lesson_categories);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`lesson_categories`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting lesson categories:'), chalk.yellow(e));
-  }
+    // inserting lesson categories
+    try {
+      await tx.insert(lesson_categories).values(data.lesson_categories);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`lesson_categories`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting lesson categories:'), chalk.yellow(e));
+    }
 
-  // inserting text lessons
-  try {
-    await db.insert(text_lessons).values(data.text_lessons);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`text_lessons`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting text lessons:'), chalk.yellow(e));
-  }
+    // inserting text lessons
+    try {
+      await tx.insert(text_lessons).values(data.text_lessons);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`text_lessons`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting text lessons:'), chalk.yellow(e));
+    }
 
-  // inserting lesson gestures
-  try {
-    await db.insert(lesson_gestures).values(data.lesson_gestures);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`lesson_gestures`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting lesson gestures:'), chalk.yellow(e));
-  }
+    // inserting lesson gestures
+    try {
+      await tx.insert(lesson_gestures).values(data.lesson_gestures);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`lesson_gestures`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting lesson gestures:'), chalk.yellow(e));
+    }
 
-  // inserting text lesson words
-  try {
-    await db.insert(text_lesson_words).values(data.text_lesson_words);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`text_lesson_words`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting text lesson words:'), chalk.yellow(e));
-  }
+    // inserting text lesson words
+    try {
+      await tx.insert(text_lesson_words).values(data.text_lesson_words);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`text_lesson_words`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting text lesson words:'), chalk.yellow(e));
+    }
 
-  // inserting user gesture recordings
-  try {
-    await db.insert(user_gesture_recordings).values(data.user_gesture_recordings);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`user_gesture_recordings`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting user gesture recordings:'), chalk.yellow(e));
-  }
+    // inserting user gesture recordings
+    try {
+      await tx.insert(user_gesture_recordings).values(data.user_gesture_recordings);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`user_gesture_recordings`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting user gesture recordings:'), chalk.yellow(e));
+    }
 
-  // inserting user gesture recording vectors
-  try {
-    await db.insert(user_gesture_recording_vectors).values(data.user_gesture_recording_vectors);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`user_gesture_recording_vectors`')
-    );
-  } catch (e) {
-    console.log(
-      chalk.red('✗ Error while inserting user gesture recording vectors:'),
-      chalk.yellow(e)
-    );
-  }
+    // inserting user gesture recording vectors
+    try {
+      await tx.insert(user_gesture_recording_vectors).values(data.user_gesture_recording_vectors);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`user_gesture_recording_vectors`')
+      );
+    } catch (e) {
+      console.log(
+        chalk.red('✗ Error while inserting user gesture recording vectors:'),
+        chalk.yellow(e)
+      );
+    }
 
-  // resetting SERIAL
-  try {
-    await db.execute(
-      sql`SELECT setval('"text_gestures_id_seq"', (select MAX(id) from "text_gestures"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"gesture_categories_id_seq"', (select MAX(id) from "gesture_categories"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"lesson_categories_id_seq"', (select MAX(id) from "lesson_categories"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"gesture_text_key_category_join_id_seq"', (select MAX(id) from "gesture_text_key_category_join"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"text_lessons_id_seq"', (select MAX(id) from "text_lessons"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"text_lesson_words_id_seq"', (select MAX(id) from "text_lesson_words"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"audio_assets_id_seq"', (select MAX(id) from "audio_assets"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"image_assets_id_seq"', (select MAX(id) from "image_assets"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"user_gesture_recordings_id_seq"', (select MAX(id) from "user_gesture_recordings"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"user_gesture_recording_vectors_id_seq"', (select MAX(id) from "user_gesture_recording_vectors"))`
-    );
-    console.log(chalk.green('✓ Successfully resetted ALL SERIAL'));
-  } catch (e) {
-    console.log(chalk.red('✗ Error while resetting SERIAL:'), chalk.yellow(e));
-  }
+    // resetting SERIAL
+    try {
+      await tx.execute(
+        sql`SELECT setval('"text_gestures_id_seq"', (select MAX(id) from "text_gestures"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"gesture_categories_id_seq"', (select MAX(id) from "gesture_categories"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"lesson_categories_id_seq"', (select MAX(id) from "lesson_categories"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"gesture_text_key_category_join_id_seq"', (select MAX(id) from "gesture_text_key_category_join"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"text_lessons_id_seq"', (select MAX(id) from "text_lessons"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"text_lesson_words_id_seq"', (select MAX(id) from "text_lesson_words"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"audio_assets_id_seq"', (select MAX(id) from "audio_assets"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"image_assets_id_seq"', (select MAX(id) from "image_assets"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"user_gesture_recordings_id_seq"', (select MAX(id) from "user_gesture_recordings"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"user_gesture_recording_vectors_id_seq"', (select MAX(id) from "user_gesture_recording_vectors"))`
+      );
+      console.log(chalk.green('✓ Successfully resetted ALL SERIAL'));
+    } catch (e) {
+      console.log(chalk.red('✗ Error while resetting SERIAL:'), chalk.yellow(e));
+    }
+  });
 };
 main().then(() => {
   queryClient.end();

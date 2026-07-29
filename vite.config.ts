2e820175bded5d565f4e119ed7af83a13e502313
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-07-29 22:13:56 +0530

    viye tsconfig nwarning resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { devtools } from '@tanstack/devtools-vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { nitro } from 'nitro/vite';
@@ -9,9 +8,11 @@ import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 const config = defineConfig({
+  resolve: {
+    tsconfigPaths: true
+  },
   plugins: [
     devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
     nitro(),

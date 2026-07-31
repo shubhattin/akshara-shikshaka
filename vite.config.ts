a619d081d941435eda6e20f89dd365436abe977c
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-07-31 08:26:50 +0530

    add back

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,11 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     nitro(),
-    viteReact()
+    viteReact({
+      babel: {
+        plugins: ['babel-plugin-react-compiler']
+      }
+    })
   ]
 });
 
